71d633d7b63840d6a52042817cae2959c60f2713
afanikolai <nik-af-01@yandex.ru>
2020-06-08 20:52:38 +0500

    Задачи 3 и 4

---
+++ Practice 3/Practice 3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_3
+{
+    class Program
+    {
+        // Дано действительное число а. Для функций, представленных ниже, вычислить f(a). 
+
+        // Условия: a - double;         При x <= 0, y = -x;     При x > 0, у = -х^2;
+
+
+
+        // Функция для вычисления f(x) по описанной выше формуле.
+        static double func(double x)
+        {
+            if (x <= 0) // Если х <= 0.
+            {
+                return -x;
+            }
+            else    // Если x > 0.
+            {
+                return -(x * x);
+            }
+
+        }
+
+        // Функция для вычисления производной функции f(x) в точке x.
+        static double der(double x)
+        {
+            if (x < 0) // Если х < 0.
+            {
+                return -1;
+            }
+            else    // Если x >= 0.
+            {
+                return -(2 * x);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Приветствие. 
+            Console.WriteLine("Учебная практика студента 1 курса НИУ ВШЭ - Пермь, Афаеасьева Н.В.\nЗадача 3 - вычисление значения " +
+                "функции f(a) c точностью в 2 знака после запятой.\n");
+
+            // Объявить переменные.
+            double a = 0;
+            bool ok = false;    // Маркер проверки правильности ввода переменной а. 
+            string tmp = "";    // Строка для ввода данных с клавиатуры.
+
+
+            while (true)    // Бесконечный цикл. 
+            {
+
+                ok = false; // Для последующих итераций. 
+
+                while (!ok) // Пока не введено правильное значение.
+                {
+                    try     // Ввод а.
+                    {
+                        Console.WriteLine("Введите действительное число а. \nЧтобы выйти введите \"close\" или \"выход\" ");
+                        Console.Write("a = ");
+                        tmp = Console.ReadLine();
+                        a = double.Parse(tmp);
+                        ok = true;
+                    }
+                    catch (Exception e)
+                    {
+                        // Массив с командами для выхода из приложения. 
+                        string[] closemas = { "Выход", "Выход ", "выход", "выход ", "Close", "close", "Close ", "close " };
+
+                        if (closemas.Contains(tmp)) // Если команда содержится в массиве. 
+                        {
+                            Environment.Exit(0);
+                        }
+                        else    // Если введена не команда. 
+                        {
+                            Console.WriteLine($"Ошибка! + {e.Message}");
+                            Console.WriteLine();
+                            ok = false;
+                        }
+                    }
+
+                }   // Пока не введено правильное значение.
+
+                // Вывести результат. 
+                Console.WriteLine($"Значение функции f(a) в точке {a} равно {func(a):f}.\nЗначение производной функции f(a) в этой же " +
+                    $"точке равно {der(a):f}. ");
+                Console.WriteLine();
+
+            }   // Бесконечный цикл.
+        }
+    }
+}
---
+++ Practice 4/Practice 4/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    // Даны натуральное число n и целые числа a[1] ... a[n], принимающие значения "0" либо "1", при том, что а[n] != 0.
+    // Последовательность a[n]*2^n + ... + a[0] задает двочиное представление целого числа p.
+    // Вернуть последовательность, задающую представление числа p - 1.
+
+
+    class Program
+    {
+
+        // Функция для отрисовки меню. 
+        static int Menu(string title, params string[] choiceOptions)    // На вход подается строка с заголовком и строковый массив с пунктами. 
+        {
+            Console.Clear();
+            Console.WriteLine(title);
+            int option = 0, x = 0, y = 2, oldOption = 0;
+            Console.CursorVisible = false;
+            for (int i = 0; i < choiceOptions.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(x, y + i);
+                Console.Write(choiceOptions[i]);
+            }
+            bool choice = false;
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition(x, y + oldOption);
+                Console.Write(choiceOptions[oldOption] + " ");
+
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(x, y + option);
+                Console.Write(choiceOptions[option]);
+
+                oldOption = option;
+
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        option++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        option--;
+                        break;
+                    case ConsoleKey.Enter:
+                        choice = true;
+                        break;
+                }
+
+                if (option >= choiceOptions.Length)
+                    option = 0;
+                else if (option < 0)
+                    option = choiceOptions.Length - 1;
+
+                if (choice)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.CursorVisible = true;
+                    Console.Clear();
+                    break;
+                }
+            }
+
+            Console.Clear();
+            Console.CursorVisible = true;
+            return option;
+        }
+
+
+        //// Функция для перевода последоваетльности нулей и единиц в десятичную систему. 
+        //static int Transfer(params int[] mas)
+        //{
+        //    int tmp = 0;
+        //    int sum = 0;
+        //    for (int i= mas.Length-1; i > -1; i--)
+        //    {
+        //        sum += (int)Math.Pow(2, tmp) * mas[i];
+        //        tmp++;
+        //    }
+        //    return sum;
+        //}
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            // Приветствие.
+            string hello = "Учебная практика студента 1 курса НИУ ВШЭ - Пермь, Афаеасьева Н.В.\nЗадача 4 - преобразование " +
+                "последовательности, задающейся в виде двоичного кода.\n\n\r";
+            Console.WriteLine(hello);
+
+            // Описание данных. 
+            string str = "";
+            int[] posl = new int[0];
+
+            // Меню выбора действия. 
+            while (true)    // Бесконечный цикл. 
+            {
+                string[] strOptions = { "1. Ввести последовательность. ","2. Вывести обработанную последовательность. ",
+                "3. Вывести закодированное в последовательности число.", "3. Выход." };
+                int option1 = Menu(hello + "Выберите действие: ", strOptions);
+                switch (option1)
+                {
+                    case 0: // Ввести последовательность.
+                        {
+                            string[] inputoptions = { "1. Из файла.", "2. С клавиатуры." };
+                            int inputoption = Menu("Выберите, как ввести последовательность. ", inputoptions);
+                            switch (inputoption)
+                            {
+                                case 0: // Из файла.
+                                    {
+                                        try
+                                        {
+                                            Console.Write("Для ввода последовательности из файла вам необходимо создать в директории " +
+                                                "...\\Practice 4\\bin\\Debug текстовый документ и заполнить его первую строку последовательностью " +
+                                                "нулей и единиц без пробелов и знаков препинания. Введите имя вашего файла ниже, или " +
+                                                "оставьте поле пустым, чтобы воспользоваться готовой последовательностью \nИмя файла: ");
+                                            string fileName = Console.ReadLine();
+
+                                            if (fileName == "") // Если имя файла пустое.
+                                            {
+                                                fileName = "input.txt";
+                                            }
+                                            else
+                                            {
+                                                // Nothing.
+                                            }
+                                            StreamReader input = new StreamReader(fileName);
+                                            str = input.ReadLine();
+                                            input.Close();
+
+                                        }
+                                        catch (Exception exception)
+                                        {
+                                            // Если ошибка, выдаем текст ошибки пользователю и возвращаемся в меню.
+                                            Console.WriteLine("\nОшибка!\n" + exception.Message + "\n");
+                                            Console.ReadLine();
+                                        }
+                                    }
+                                    break;
+
+                                case 1: // С клавиатуры. 
+                                    {
+                                        try
+                                        {
+                                            Console.Write("Для ввода последовательности c клавиатуры не используйте пробел и " +
+                                                "знаки препинания. \nВаша последовательность: ");
+                                            str = Console.ReadLine();
+                                        }
+                                        catch (Exception exception)
+                                        {
+                                            // Если ошибка, выдаем текст ошибки пользователю и возвращаемся в меню.
+                                            Console.WriteLine("\nОшибка!\n" + exception.Message + "\n");
+                                            Console.ReadLine();
+                                        }
+                                    }
+                                    break;
+                            }
+
+
+                        }
+                        break;
+
+                    case 1: // Вывести обработанную последовательность.
+                        {
+                            Console.Clear();
+                            try
+                            {
+                                // Преобразование строки двоичного кода в целое число.
+                                int ch = Convert.ToInt32(str, 2) - 1;
+
+
+                                if (ch >= 0)    // Если результат положителен.
+                                {
+                                    // Вывод преобразованного в строку двоичного кода результата.
+                                    Console.WriteLine(Convert.ToString(ch, 2));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ошибка! Число должно быть больше нуля. ");
+                                }
+                                Console.ReadLine();
+                            }
+                            catch (Exception exception)
+                            {
+                                // Если ошибка, выдаем текст ошибки пользователю и возвращаемся в меню.
+                                Console.WriteLine("\nОшибка!\n" + exception.Message + "\n");
+                                Console.ReadLine();
+                            }
+                        }
+                        break;
+
+                    case 2: // Вывести закодированное в последовательности число.
+                        {
+                            Console.Clear();
+                            try
+                            {
+                                // Преобразование строки двоичного кода в целое число.
+                                int ch = Convert.ToInt32(str, 2);
+
+                                if (ch >= 0)    // Если результат положителен.
+                                {
+                                    // Вывод целого числа.
+                                    Console.WriteLine(ch);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ошибка! Число должно быть больше нуля. ");
+                                }
+                                Console.ReadLine();
+                            }
+                            catch (Exception exception)
+                            {
+                                // Если ошибка, выдаем текст ошибки пользователю и возвращаемся в меню.
+                                Console.WriteLine("\nОшибка!\n" + exception.Message + "\n");
+                                Console.ReadLine();
+                            }
+                        }
+                        break;
+
+                    case 3: // Выход.
+                        {
+                            Environment.Exit(0);
+                        }
+                        break;
+                }
+
+            }   // Бесконечный цикл. 
+
+        }
+    }
+}
